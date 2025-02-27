80590a149fb94f82b4b926440399cc2e8972fe3b
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-02-27 17:01:44 -0300

    atualizações17h01

---
+++ ExercicioAPIdeCurso/ExercicioAPIdeCurso/src/test/java/com/example/ExercicioAPIdeCurso/ExercicioApIdeCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExercicioAPIdeCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApIdeCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
